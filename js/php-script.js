0febd360e71e9550cab50ddfa9aabc5b4ee516fb
Florian <florianwachter@gmx.net>
2018-05-28 12:12:48 +0200

    added mail sender.php

--- js/php-script.js
+++ js/php-script.js
@@ -2,44 +2,38 @@
 // bestellen    
 // --------------------------------------------
 
-
-
-// information: .info-date
-// send request btn: .reg-btn
-
-
 var path;
 // Button Click
 $('.reg-btn').on('click', function(event) {
-    path =  $(this).parent().parent(); // form-tag
-
-
-    var valid = true;
-
-$(path).find("input, select").each(function  () {
-        if(!$(this)[0].checkValidity()){
-            valid = false;
-        }
-});
-    if(valid){
-        bestellen();
-        // $('.js-bestellung-erfolgt').addClass('is-visible');
+    var $this = $(this).parent().parent(); // im Form
+    var info_date = $this.find('.info-date').text();
+    var info_mail = $this.find('.info-mail').val();
+    
+    console.log('Registration!');
+    console.log('date: ' + info_date );
+    console.log('mail: ' + info_mail );
+      
+    if( $($this)[0].checkValidity() ){
+      // true 
+      console.log('Email OK --S senden');
+      bestellen(info_date, info_mail);
+    } else {
+      // false = keine mail adresse
+      console.log('Email error');
     }
+
 });
 
-
-function bestellen() {
-
+function bestellen(date, mail) {
     $.ajax({   
           url: 'php/send.php',   
           type: 'POST',
           data: {
-                    mail            : $(path).find('.info-date').val(),
-                    date            : $(path).find('.info-mail').val(),
+                  mail  : date,
+                  date  : mail,
               },
           success: function(resp){
             console.log(resp);
-    
           },
           error: function(resp){
             console.log(resp.responseText);
--- php/send.php
+++ php/send.php
@@ -1,115 +1,73 @@
 <?php
-
-
     // Only process POST reqeusts.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
     
-    
-    $recipient        = "Adus mail adress" 
-    $mail             = $_POST['mail']; // mail address from registration
-    $date             = $_POST['date']; // mail address from registration
+    $basemail        = "info@vaginalove.ch"; // website mail-adress
+    $date             = $_POST['date']; // data from website
+    $mail_customer             = $_POST['mail']; // data from website
 
     
-    $subject = 'Anmeldung';
-    $headers = "From: " . $mail . "\r\n";
+    //-------------------------------------------------------
+    // Mail to Webserver Host 
+    //-------------------------------------------------------
+    $subject = 'Neue Anmeldung';
+    $headers = "From: " . $mail_customer . "\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=iso 8859-1\r\n";
 
     $message = '<html><body>';
-    $message .= "Danke für die anmeldung am ".$date.". Du wirst in Kürze eine Mail mit weiteren Informationen erhalten.";
+    $message .= "Neue Anmeldung am ".$date.". Mail adresse: ".$mail_customer;
     $message .= "</body></html>";
 
+    //-------------------------------------------------------
+    // Mail to Customer
+    //-------------------------------------------------------
+    $subject_customer = 'Anmeldung';
+    $headers_customer = "From: " . $basemail . "\r\n";
+    $headers_customer .= "MIME-Version: 1.0\r\n";
+    $headers_customer .= "Content-Type: text/html; charset=iso 8859-1\r\n";
+
+    $message_customer = '<html><body>';
+    $message_customer .= "<h3>Danke für die Anmeldung am ".$date.". Du wirst in Kürze eine Mail mit weiteren Informationen erhalten.</h3>";
+    $message_customer .= "</body></html>";        
+
         //-------------------------------------------------------
+        // Mail to Webserver Host  - Send the email.
+        //-------------------------------------------------------
+        if ( mail($basemail, $subject, $message, $headers) ) {
+                    // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "email was sent to:".$basemail;
+        } else {
+                    // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+        
+        //-------------------------------------------------------
+        // Mail to Customer - Send the email.
+        //-------------------------------------------------------
+        if ( mail($mail_customer, $subject_customer, $message_customer, $headers_customer) ) {
+                    // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "email was sent to: ".$mail_customer;
+        } else {
+                    // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
 
 
-        // Order Text to Customer.
-$subject_customer = l::get('bwestellung') . "ACUSTICA";
-
-$headers_customer = "From: " . $recipient . "\r\n";
-        //$headers_customer .= "Reply-To: ". $recipient . "\r\n";
-$headers_customer .= "MIME-Version: 1.0\r\n";
-$headers_customer .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-$message_customer = '<html><body>';
-$message_customer .= '<h1>ACUSTICA ' . l::get('bestellung') . '</h1>';
-$message_customer .= '<table rules="all" style="border-color: #000;" >';
-$message_customer .= "<tr style='background: #fff;'>
-<td>
-    <strong>Adresse:</strong> 
-</td>
-<td>" . $anrede . "<br>"
-   . $vname . "<br>"
-   . $name . "<br>"
-   . $adresse . "<br>"          
-   . $adresszusatz . "<br>"     
-   . $plz . "<br>"              
-   . $ort . "<br>"              
-   . $land . "<br>" 
-   . $telefonnummer . 
-   "<br>
-</td>";                             
-$message_customer .= "<tr style='background: #fff;'>
-<td>
-    <strong>" . l::get('bestellung') . ":</strong> 
-</td>
-<td>" 
-    . l::get('zifferblatt'). ": " .  $zifferblatt . "<br>" 
-    . l::get('armband') . ": " .  $armband . "<br>" 
-    . l::get('sprache') . ": ".   $sprache . "<br>" 
-    . l::get('sprachtype')  . ": ".$sprachtype . "<br>" 
-    . l::get('zeitausgabe') . ": ". $zeitausgabe . 
-    "</td>
-</tr>";
-
-$message_customer .= "</table>";
-$message_customer .= "</body></html>";
+        $data = $date." ".$mail_customer;
+    echo("<script>console.log('PHP: ".$data."');</script>");
 
 
 
-
-
-
-
-
-
-
-        // Build the email headers.
-$email_headers = "Bestellung: $name <$email>";
-
-        // Send the email.
-if ( mail($recipient, $subject, $message, $headers) ) {
-            // Set a 200 (okay) response code.
-    http_response_code(200);
-    echo "email was sent to: $recipient ";
-} else {
-            // Set a 500 (internal server error) response code.
-    http_response_code(500);
-    echo "Oops! Something went wrong and we couldn't send your message. --> $send";
-}
-
-
-
-        // Send the email.
-if ( mail($mail, $subject_customer, $message_customer, $headers_customer) ) {
-            // Set a 200 (okay) response code.
-    http_response_code(200);
-    echo "email was sent to: $recipient";
-} else {
-            // Set a 500 (internal server error) response code.
-    http_response_code(500);
-    echo "Oops! Something went wrong and we couldn't send your message. --> $send";
-}
-
-
-
-
-
-
-} else {
-        // Not a POST request, set a 403 (forbidden) response code.
-    http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
-    go("home");
-}
+    } else {
+            // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
 
 ?>
