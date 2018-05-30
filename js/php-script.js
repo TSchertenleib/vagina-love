b84f76a7fb6e2cf8858f5f5fb3bfaa5db046f26a
Florian <florianwachter@gmx.net>
2018-05-30 13:00:53 +0200

    php mail two ways

--- js/php-script.js
+++ js/php-script.js
@@ -6,44 +6,37 @@ var path;
 // Button Click
 $('.reg-btn').on('click', function(event) {
     var $this = $(this).parent().parent(); // im Form
-    
     var info_date = $this.find('.info-date').text();
-    
     var info_mail = $this.find('.info-mail').val();
+    var info_name = $this.find('.info-name').val();
     
-    
-    
-    
-    // $($this).find("input, select").each(function  () {
-    //   if(!$(this)[0].checkValidity()){
-    //     valid = false;
-    //   }
-    // }); 
 
     if( $($this)[0].checkValidity() ){
       // true 
-      bestellen(info_date, info_mail);
-      console.log('sent data to php: ' + info_mail+","+info_date+"\n");
+      bestellen(info_date, info_mail, info_name);
+       console.log('sent data to php: ' + info_mail+","+info_date+","+info_name+"\n");
     } else {
-      // false = keine mail adresse
       console.log('Email error');
     }
-
 });
 
-function bestellen(date, mail) {
+
+function bestellen(date, mail, vname) {
     $.ajax({   
           url: 'php/send.php',   
           type: 'POST',
           data: {
                   date  : date,
                   mail  : mail,
+                  vname  : vname,
               },
           success: function(resp){
-            console.log(resp);
+            //console.log(resp);
+            console.log('success');
           },
           error: function(resp){
-            console.log(resp.responseText);
+            //console.log(resp.responseText);
+            console.log('erorr');
           }
       });  
 }
--- php/send.php
+++ php/send.php
@@ -12,38 +12,78 @@ require 'Exception.php';
 require 'PHPMailer.php';
 require 'SMTP.php';
 
+$date            = $_POST['date']; // data from website
+$mail_customer   = $_POST['mail']; // data from website
+$vname           = $_POST['vname']; // data from website
+$message = $vname." mit der email: ".$mail_customer." nimmt am ".$date." teil";
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+
+
+//Wird an angemeldeten gesendet
+$mail_cus = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'send.one.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'anmeldung@vaginalove.ch';                 // SMTP username
-    $mail->Password = 'adou1992';                    // SMTP password
-    $mail->SMTPSecure = 'ssl';                       // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465;                               // TCP port to connect to
+    $mail_cus->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail_cus->isSMTP();                                      // Set mailer to use SMTP
+    $mail_cus->Host = 'send.one.com';  // Specify main and backup SMTP servers
+    $mail_cus->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail_cus->Username = 'anmeldung@vaginalove.ch';                 // SMTP username
+    $mail_cus->Password = 'adou1992';                    // SMTP password
+    $mail_cus->SMTPSecure = 'ssl';                       // Enable TLS encryption, `ssl` also accepted
+    $mail_cus->Port = 465;                               // TCP port to connect to
     //Recipients
-    $mail->setFrom('anmeldung@vaginalove.ch', 'vaginalove');
+    $mail_cus->setFrom('anmeldung@vaginalove.ch', 'vaginalove');
     
 
-    // GET DATA
-    //$basemail        = "info@vaginalove.ch"; // website mail-adress
-    $date            = $_POST['date']; // data from website
-    $mail_customer   = $_POST['mail']; // data from website
-    $message = $mail_customer." nimmt am ".$date." teil";
+    // GET DAT
     echo("<script>console.log('PHP-log: ".$message."');</script>");
     // GET DATA ENDE
 
-    $mail->addAddress($mail_customer);               // Name is optional
+    $mail_cus->addAddress($mail_customer);               // Name is optional
+    $mail_cus->isHTML(true);                                  // Set email format to HTML
+    $mail_cus->Subject = 'Danke für ihre anmeldung bei VaginaLove am ...';
+    $mail_cus->Body    = $message;
+    $mail_cus->AltBody = $message;
 
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail_cus->send();
+    echo 'Message has been sent to:'.$mail_customer;
+    
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
 
-    $mail->send();
+
+// Wird an host fvon der website gesendet
+$mail_host = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail_host->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail_host->isSMTP();                                      // Set mailer to use SMTP
+    $mail_host->Host = 'send.one.com';  // Specify main and backup SMTP servers
+    $mail_host->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail_host->Username = 'anmeldung@vaginalove.ch';                 // SMTP username
+    $mail_host->Password = 'adou1992';                    // SMTP password
+    $mail_host->SMTPSecure = 'ssl';                       // Enable TLS encryption, `ssl` also accepted
+    $mail_host->Port = 465;                               // TCP port to connect to
+    //Recipients
+    $mail_host->setFrom('anmeldung@vaginalove.ch', 'vaginalove');
+    
+
+
+        
+    $message_host = "name:".$vname."\n emial: ".$mail_customer."\n nimmt tiel: ".$date;
+
+
+    echo("<script>console.log('PHP-log: ".$message."');</script>");
+    // GET DATA ENDE
+    $mail_host->addAddress('info@vaginalove.ch');               // Name is optional
+
+    $mail_host->isHTML(true);                                  // Set email format to HTML
+    $mail_host->Subject = 'Neue Anmeldung für'.$date;
+    $mail_host->Body    = $message_host;
+    $mail_host->AltBody = $message_host;
+
+    $mail_host->send();
     echo 'Message has been sent to:'.$mail_customer;
     
 } catch (Exception $e) {
@@ -52,7 +92,6 @@ try {
 
 
 
-   
 
 
 
